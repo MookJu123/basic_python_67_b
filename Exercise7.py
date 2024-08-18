6bc6717e31b25f49822c9be6fab751d259b95aca
Jutharat <Jutharatmook55@gmail.com>
2024-08-18 16:10:45 +0700

    Exercise7 Uplord

--- Exercise7.py
+++ Exercise7.py
@@ -9,5 +9,8 @@ for num in numbers:
     if num % 2 == 0:
         even_numbers.append(num)  # ถ้าเลขหาร 2 ลงตัว (เป็นจำนวนคู่) ให้เพิ่มลงใน even_numbers
     else:
-        odd_numbers.append(num)
-        
+        odd_numbers.append(num)     # ถ้าเลขหาร 2 ไม่ลงตัว (เป็นจำนวนคี่) ให้เพิ่มลงใน odd_numbers
+
+print("เลขคี่:", odd_numbers)
+print("เลขคู่:", even_numbers)
+    
