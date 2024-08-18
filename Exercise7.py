e42608c4ac082c7522b03da3ddfda3b3cb9b6a88
Jutharat <Jutharatmook55@gmail.com>
2024-08-18 15:58:25 +0700

    Exercise7 Uplord

---
+++ Exercise7.py
@@ -0,0 +1,13 @@
+# สร้างลิสต์ของตัวเลขจาก 21 ถึง 40
+numbers = list(range(21, 41))
+
+# แยกจำนวนคี่และจำนวนคู่
+odd_numbers = []
+even_numbers = []
+
+for num in numbers:
+    if num % 2 == 0:
+        even_numbers.append(num)  # ถ้าเลขหาร 2 ลงตัว (เป็นจำนวนคู่) ให้เพิ่มลงใน even_numbers
+    else:
+        odd_numbers.append(num)
+        
